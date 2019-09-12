46bc0f85d4038df5c2a0c89f66dff61aaf87387d
Melissa Schyns <melsia10@hotmail.com>
2019-09-12 16:50:41 +0200

    application runner

--- src/main/java/be/technifutur/MVC/MVCFramework.java
+++ src/main/java/be/technifutur/MVC/MVCFramework.java
@@ -1,14 +1,18 @@
 package be.technifutur.MVC;
 
 
+import be.technifutur.MVC.applicationRunner.InteractiveRunner;
 
-public class MVCFramework extends ApplicationMVC {
-    ApplicationMVC applicationMVC;
+public class MVCFramework<T> {
+    ApplicationMVC applicationMVC = new ApplicationMVC();
     public void start(MVCFactory app){
+        InteractiveRunner interactiveRunner = new InteractiveRunner();
         applicationMVC.setControleur(app.getControleur());
         applicationMVC.setModel(app.getModel());
         applicationMVC.setVue(app.getVue());
         applicationMVC.start();
+        interactiveRunner.setApplication(applicationMVC);
+        interactiveRunner.run();
     }
 
 }
